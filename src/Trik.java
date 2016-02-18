dc2a060ca0282b2cedba6b71107a71116ccde1fe
BenzHD <BenzHD@BenzHD-PC>
2016-02-19 00:05:22 +0700

    Homwork

--- src/Trik.java
+++ src/Trik.java
@@ -1,41 +1,37 @@
 import java.util.Scanner;
-
 public class Trik {
+	static int p = 1;
+	static char abc[] = new char[100];
 	public static void main(String[] args) {
-		String ch;
-		int A = 1;
-		int B = 0;
-		int C = 0;
-		int temp=0;
-		Scanner scan = new Scanner(System.in);
-		do {
-			System.out.print("Enter character (A,B or C) : ");
-			ch = scan.nextLine();
-			if(ch.length() > 50){
-				System.out.println("Invalid input. Please enter again!!!");
-			}
-		} while (ch.length() > 50000);
-		for (int i = 0; i < ch.length(); i++) {
-			if (ch.charAt(i) == 'A' || ch.charAt(i) == 'a') {
-				temp = A;
-				A = B;
-				B = temp;
-			} else if (ch.charAt(i) == 'B' || ch.charAt(i) == 'b') {
-				temp = B;
-				B = C;
-				C = temp;
-			} else if (ch.charAt(i) == 'C' || ch.charAt(i) == 'c') {
-				temp = A;
-				A = C;
-				C = temp;
-			}
-		}
-		if (A == 1) {
-			System.out.println("Ball in glass : 1");
-		} else if (B == 1) {
-			System.out.println("Ball in glass : 2");
-		} else if (C == 1) {
-			System.out.println("Ball in glass : 3");
-		}
+		Input();
+		Cal();
+		Display();
 	}
+	
+	public static void Input(){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input Data : ");
+		abc=scan.next().toCharArray();
+	}
+	
+	public static void Cal(){
+    	for (int i = 0 ; i < abc.length ; i++) {
+			if (abc[i]=='A') {
+				if (p==1) { p=2; }
+				else if (p==2) { p=1;}
+			}
+			else if (abc[i]=='B') {
+				if (p==2) { p=3; }
+				else if (p==3) { p=2;}
+			}
+			else if (abc[i]=='C') {
+				if (p==3) { p=1; }
+				else if (p==1) { p=3;}
+			}
+		}	
+	}
+
+    public static void Display(){
+    	System.out.print("Show Data : " + p);
+    }
 }
